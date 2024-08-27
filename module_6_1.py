7ac509d1e3f038ab2260e9b19160b8b9c8b7628b
ShumsAn <shum-sol@outlook.com>
2024-08-27 23:35:18 +0300

    Задача "Съедобное, несъедобное"
    Домашнее задание по теме "Зачем нужно наследование"
    Цель: применить базовые знания о наследовании классов для решения задачи

---
+++ module_6_1.py
@@ -0,0 +1,71 @@
+class Animal:
+    """ Родительскй класс
+    Где атрибуты alive = True(живой) и fed = False(накормленный), name - индивидуальное название каждого животного."""
+
+    alive = True
+    fed = False
+
+    def __init__(self,name):
+        self.name = name
+
+class Mammal(Animal):
+    """Наследуемый класс с методом eat(self, food)  где food - это параметр, принимающий объекты классов растений"""
+
+    def eat(self, food):
+        self.food = (Plant)
+        if food.edible == True:
+            print(f'{self.name},съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name}, не стал есть {food.name}')
+            self.alive = False
+        return self.fed, self.alive
+
+class Predator (Animal):
+    """Наследуемый класс с методом eat(self, food)  где food - это параметр, принимающий объекты классов растений"""
+    def eat(self, food):
+        self.food = (Plant)
+        if food.edible == True:
+            print(f'{self.name},съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name}, не стал есть {food.name}')
+            self.alive = False
+        return self.fed , self.alive
+
+class Plant:
+    """ Родительскй класс Растения
+        Где атрибут edible = False(съедобность), name - индивидуальное название каждого растения"""
+
+    edible = False
+
+    def __init__(self, name):
+        self.name = name
+
+class Fruit(Plant):
+    """Наследуемый класс с атрибутом edible - сьедобность"""
+    edible = True
+
+class Flower(Plant):
+    """Наследуемый класс с атрибутом edible - сьедобность"""
+    edible = False
+
+
+# Проверка
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+# Что произошло: Хищник попытался съесть цветок и погиб, млекопитающее съело фрукт и насытилось.
